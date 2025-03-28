d0583c79983adcadaf8e203d3b2c89894a6606d7
BroPlasmaNinja <HeadMenlvl100@yandex.ru>
2025-03-28 19:21:50 +0400

    preRelise

--- Assets/Scripts/RunScripts/ScriptablesObjects/SpellInfo.cs
+++ Assets/Scripts/RunScripts/ScriptablesObjects/SpellInfo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assets.Scripts.RunScripts.Info
+namespace Assets.Scripts.RunScripts.ScriptableObjects
 {
     internal class SpellInfo
     {
--- Assets/Scripts/RunScripts/ScriptablesObjects/Waves.cs
+++ Assets/Scripts/RunScripts/ScriptablesObjects/Waves.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEditor;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts.RunScripts.Interfaces;
 
-namespace Assets.Scripts.RunScripts.Info
+namespace Assets.Scripts.RunScripts.ScriptableObjects
 {
-    internal class Waves : ScriptableObject
+    internal class Waves : ScriptableObject, IAvailableWithProgress
     {
+        [SerializeField] 
         List<(EnemyInfo, int Count)> Enemies;
+        [SerializeField] 
+        uint MinTimer;
+        [SerializeField] 
+        uint MaxTimer;
+        [SerializeField]
+        public ulong _necessarySouls;
+        public ulong necessarySouls => _necessarySouls;
     }
 }
