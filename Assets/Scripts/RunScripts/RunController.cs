b1d1e72c3b0bca5fd9bc62b3b35dac78ce08e7d1
BroPlasmaNinja <HeadMenlvl100@yandex.ru>
2025-03-28 17:48:10 +0400

    minor changes in RunController

--- Assets/Scripts/RunScripts/RunController.cs
+++ Assets/Scripts/RunScripts/RunController.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class RunController : MonoBehaviour
+public sealed class RunController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    uint Souls;
+    readonly uint[] LevelUpBorders;
+    uint Timer;
+    void OnEnd()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+    void OnPlayerDeath()
+    {
+
+    }
+    float difficultyCoef;
 }
