ae257ae463a935272d08a81a4bc720b61ce41dce
Pipiaka <milyushkin02@mail.ru>
2025-03-28 18:43:42 +0400

    The circle is mooooovegit add .git add .

--- Assets/Scripts/RunScripts/Player.cs
+++ Assets/Scripts/RunScripts/Player.cs
@@ -7,7 +7,8 @@ namespace Assets.Scripts.RunScripts
 {
     public sealed class Player : MonoBehaviour, ICanCast, IDamagable
     {
-        [Serializable] private float _speed;
+        [SerializeField]
+        private float _speed;
 
         public event EventHandler death;
 
@@ -29,6 +30,11 @@ namespace Assets.Scripts.RunScripts
         {
             gameObject.transform.position += new Vector3(0, _speed * Input.GetAxis("Vertical"), 0);
             gameObject.transform.position += new Vector3(_speed * Input.GetAxis("Horizontal"), 0, 0);
+        }
+
+        public void Update()
+        {
+            Move();
         }
 
     }
