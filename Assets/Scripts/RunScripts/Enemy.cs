274e16b65243b4cf2cff2c160b3cbe2d48840c0d
Pipiaka <milyushkin02@mail.ru>
2025-03-29 07:30:08 +0400

    Implemented class Enemy and Ai

--- Assets/Scripts/RunScripts/Enemy.cs
+++ Assets/Scripts/RunScripts/Enemy.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Assets.Scripts.RunScripts.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.RunScripts
 {
-    internal class Enemy
+    public class Enemy : MonoBehaviour, IDamagable
     {
+        [SerializeField] private float _speed;
+
+        [SerializeField] private GameObject _player;
+
+        public event EventHandler death;
+
+        public void Death()
+        {
+
+        }
+
+        public void TakeDMG()
+        {
+
+        }
+
+        public void AI()
+        {
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, _player.transform.position, _speed*Time.deltaTime);
+        }
+
+        public void Update()
+        {
+            AI();
+        }
     }
 }
