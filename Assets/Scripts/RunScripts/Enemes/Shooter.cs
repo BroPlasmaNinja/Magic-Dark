84924751eb0f9aebe7fc60d1351332111d58019c
Pipiaka <milyushkin02@mail.ru>
2025-03-29 16:28:37 +0400

    Add immortality for player

--- Assets/Scripts/RunScripts/Enemes/Shooter.cs
+++ Assets/Scripts/RunScripts/Enemes/Shooter.cs
@@ -17,7 +17,6 @@ namespace Assets.Scripts.RunScripts
 
         public override void AI()
         {
-            Debug.Log((Player.ins.transform.position-gameObject.transform.position).magnitude);
             if ((Player.ins.transform.position - gameObject.transform.position).magnitude > _radiuse) gameObject.transform.position += Vector3.Normalize(Player.ins.transform.position - gameObject.transform.position) * _enemyInfo.Speed * Time.deltaTime;
         }
 
