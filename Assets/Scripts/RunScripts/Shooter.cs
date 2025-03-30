724ba8d6467b3adf8b58397013c4bf0a57050614
BroPlasmaNinja <HeadMenlvl100@yandex.ru>
2025-03-30 08:05:37 +0400

    daaaaaaaaa

--- Assets/Scripts/RunScripts/Shooter.cs
+++ Assets/Scripts/RunScripts/Shooter.cs
@@ -40,13 +40,13 @@ public class Shooter : MonoBehaviour
         {
             Debug.Log("Amogus");
             yield return new WaitForSeconds(spell.state.cooldown);
-            if(Enemies.Count!=0)
+            if (Enemies.Count != 0)
                 for (int i = 0; i < spell.state.countProjectiles; i++)
                 {
                     GameObject go = spell.CreateObject(null);
-                    var VectorAfterSpread = (((closestEnemy.transform.position - transform.position).normalized.y / (closestEnemy.transform.position - transform.position).normalized.x) + GameManager.rnd.Next(-(int)spell.state.spreadAngle, (int)spell.state.spreadAngle));
-                    go.GetComponent<Rigidbody2D>().AddForce(new Vector2(Mathf.Cos(VectorAfterSpread), Mathf.Sin(VectorAfterSpread)) * spell.state.speed);
+                    var GoalVector = (closestEnemy.transform.position - transform.position).normalized;
                     go.transform.position = transform.position;
+                    go.GetComponent<Rigidbody2D>().velocity = new Vector2(GoalVector.x, GoalVector.y);
                     yield return new WaitForSeconds(spell.state.waitBetweenProjectileMs / 1000);
                 }
         }
